0eb5f43e54dd1c1eea4a6730a803f87bcc23c4da
XicoPi <5172714+XicoPi@users.noreply.github.com>
2020-12-15 21:43:45 +0100

    gen keys function implementation

--- python/rsa.py
+++ python/rsa.py
@@ -19,8 +19,31 @@ def genPrime(iterationNumber, N):
             i = 0
     tf = time.time()
     return (tf - t0, primeTest)
+
+def modularInv(num, mod):#repasar algorisme
+    k = 0
+    inv = (1 + mod*k)/num
+    while (inv % 1 != 0.0 and int(inv) < mod):
+        print(inv)
+        k += 1
+        inv = (1 + mod*k)/num
+    if (int(inv) > mod):
+        inv = -1
+    return int(inv)
+
+def genKeys(nBit):
+    iterationNumber = 100
+    q = genPrime(iterationNumber,nBit/2)[1]
+    p = genPrime(iterationNumber,nBit/2)[1]
+    n = p * q
     
+    d = genCandidate(nBit)
+    e = modularInv(d, (p-1)*(q-1))
+    while (e < 0):
+        d = genCandidate(nBit)
+        e = modularInv(d, (p-1)*(q-1))
+    return {"private":(q,p,d), "public": (e, n)}
 if __name__ == '__main__':
 
-    N = 2048 #nuber of binary digits (bits). Example: RSA-100 -> N = 330 bits
-    print (str(genPrime(100,N)))
+    N = 330 #nuber of binary digits (bits). Example: RSA-100 -> N = 330 bits
+    print (str(genKeys(N)))
