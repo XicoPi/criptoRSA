dc5cd293d449428dcd4932aee9ca8de0d0464195
XicoPi <5172714+XicoPi@users.noreply.github.com>
2021-01-15 13:52:44 +0100

    modular inverse implementation

---
+++ python/modInv.py
@@ -0,0 +1,20 @@
+import math
+
+def modInv(a, module):
+    result = 0
+    if (math.gcd(a,module) == 1):
+        residue = module
+        y = 0
+        x = 1
+        
+        while (a > 1):
+            quotient = int(a / residue)
+            temp = residue
+
+            residue = a % residue
+            a = temp
+            temp = y
+            y = x - quotient * y
+            x = temp
+        result = x % module
+    return result
